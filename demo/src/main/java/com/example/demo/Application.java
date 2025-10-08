01787d9d20e08fe7bdbfa3eddd5b0c979e8da410
Michael Faria <michaelronaldfaria@gmail.com>
2025-10-08 15:18:47 +0100

    Created a simple web service that accepts HTTP GET requests and responds with a JSON representation of "Hello World!"

--- demo/src/main/java/com/example/demo/Application.java
+++ demo/src/main/java/com/example/demo/Application.java
@@ -6,12 +6,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @SpringBootApplication
-@RestController // tells Spring that this code describes an endpoint that should be made available over the web
+
+// HTTP requests are handled by a controller, this tells Spring that this code describes an endpoint that should be made available over the web
+@RestController
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+
+    // code below creates a simple service accepting HTTP GET requests at http://localhost:8080/hello and respond with a JSON representation of "Hello, World!"
+
+
+    // this "annotation" ensures that HTTP GET requests to "/greeting" are mapped to the greeting() method
+    @GetMapping("/greeting")
+
+    // @RequestParam binds the value of the query string into the parameter of the greeting method. Both parameters of the annotation are optional
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+
+    // code below creates a simple "Hello, World!" endpoint at http://localhost:8080/hello
+
 
     // this line of code is an "annotation", this @GetMapping essentially will execute the method below to the address allocated in the param,
     // (http://localhost:8080/hello) in this case. an empty string ("") would send the request to http://localhost:8080
