8b258bc0d5d2af2b366799f02daa5a1859fa9b51
Michael Faria <michaelronaldfaria@gmail.com>
2025-10-12 13:13:13 +0100

    Added setters to Task.java, for future update operations.

--- src/main/java/com/example/demo/Entity/Task.java
+++ src/main/java/com/example/demo/Entity/Task.java
@@ -48,15 +48,31 @@ public class Task {
         return title;
     }
 
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getDescription() {
         return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getStatus() {
         return status;
     }
 
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     public LocalDate getDueDate() {
         return dueDate;
+    }
+
+    public void setDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
     }
 }
