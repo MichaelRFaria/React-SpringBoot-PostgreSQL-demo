78293aa7c738f7b0f997eb8846eb176797ea94d0
Michael Faria <michaelronaldfaria@gmail.com>
2025-10-13 15:18:10 +0100

    Created React app. Refactored the project root to be split between backend and frontend.

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+// to run type the following into the cmd in the frontend directory:
+// npm start
+// ctrl+c to stop
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
