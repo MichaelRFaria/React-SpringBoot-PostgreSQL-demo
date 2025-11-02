841fdce333d1e644e0d1ee2706ac7bc5c68158bb
Michael Faria <michaelronaldfaria@gmail.com>
2025-11-02 12:00:14 +0000

    Added buttons to filter tasks by status and due date.

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -5,8 +5,11 @@ import {useEffect, useState} from "react";
 export default function Tasks({tasks}) {
     const [listOfTasks, setListOfTasks] = useState([]);
     const [sortValue, setSortValue] = useState("id");
+    const [filterConstraints, setFilterConstraints] = useState({ completed: true, uncompleted: true, beforeDueDate: true, overDue: true});
+    // we need the date to filter by before due date and overdue.
+    const [localDate, setLocalDate] = useState("");
 
-    // hook to
+    // hook to sort tasks and map them to list items
     useEffect(() => {
         // React has no clue how to display an object, so we need to map the object, to something that React can recognise
         // in this case it's the data members of the Task object, which consist of: Integer, String, and LocalDate, which React understands
@@ -33,6 +36,23 @@ export default function Tasks({tasks}) {
 
     },[tasks, sortValue])
 
+    useEffect(() => {
+        setLocalDate(new Date().toLocaleDateString());
+    },[])
+
+    // useEffect(() => {
+    //     console.log(localDate);
+    // }, [localDate]);
+
+    // function to update checkbox state object
+    const handleFilter = (e) => {
+        // using a callback function in the setState method will give you the previous state (can be called anything)
+        // ...prevState "spreads" (copies) the old properties into a new object
+        // then we add (or override if it already exists) the new object
+        setFilterConstraints(prevState => ({...prevState, [e.target.name]: e.target.checked}));
+    }
+
+
     return (
         <div id={"displayedTasks"}>
             <h3>Your tasks:</h3>
@@ -46,6 +66,25 @@ export default function Tasks({tasks}) {
                     <option value="dueDate">Date Due</option>
                 </select>
             </label>
+
+            <p>Filter Tasks:</p>
+            <label>
+                Completed:
+                <input type="checkbox" name="completed" checked={filterConstraints.completed} defaultChecked={true} onChange={handleFilter}/>
+            </label>
+            <label>
+                Uncompleted:
+                <input type="checkbox" name="uncompleted" checked={filterConstraints.uncompleted} defaultChecked={true} onChange={handleFilter}/>
+            </label>
+            <label>
+                Before due date:
+                <input type="checkbox" name="beforeDueDate" checked={filterConstraints.beforeDueDate} defaultChecked={true} onChange={handleFilter}/>
+            </label>
+            <label>
+                Overdue:
+                <input type="checkbox" name="overdue" checked={filterConstraints.overdue} defaultChecked={true} onChange={handleFilter}/>
+            </label>
+
             <hr></hr>
 
             {tasks.length !== 0 ? (
