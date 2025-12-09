acd062bc37a8bc45f12bac58afe0de84e300ed64
Michael Faria <michaelronaldfaria@gmail.com>
2025-12-09 11:32:32 +0000

    Modified Notification.jsx component to use improved visibility transition logic, seen in Alert.jsx

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -3,7 +3,7 @@ import '../styles/Notification.css'
 export default function Notification({message, isVisible}) {
 
     return (
-        <div id={`notification${isVisible ? 'Visible' : 'Invisible'}`}>
+        <div id="notification" className={isVisible ? "visible" : ""}>
             {message}
         </div>
     )
