38673644ab1d15a12411f95c9c1993b2165ad349
Michael Faria <michaelronaldfaria@gmail.com>
2025-12-15 12:28:39 +0000

    Improved styling, clarity and normalised the structure of the options in the FilterOptions.jsx component.

--- frontend/src/components/FilterOptions.jsx
+++ frontend/src/components/FilterOptions.jsx
@@ -14,14 +14,14 @@ export default function FilterOptions({
                 <div id={"searching"}>
                     <label>
                         <h4>Search for Task:</h4>
-                        Search for:
-                        <input name="searchValue" onChange={e => setSearchValue(e.target.value)}/>
+                        <p>Search for:</p>
+                        <input name="searchValue" placeholder="Enter a keyword." onChange={e => setSearchValue(e.target.value)}/>
                     </label>
                 </div>
                 <div id={"sorting"}>
                     <label>
                         <h4>Sort Tasks:</h4>
-                        Sort by:
+                        <p>Sort by:</p>
                         <select value={sortValue} onChange={e => setSortValue(e.target.value)}>
                             <option value="id">ID</option>
                             <option value="title">Title</option>
@@ -35,6 +35,7 @@ export default function FilterOptions({
 
                 <div id={"filtering"}>
                     <h4>Filter Tasks:</h4>
+                    <p>Only show:</p>
                     <label>
                         Completed:
                         <input type="checkbox" name="completed" checked={filterConstraints.completed}
