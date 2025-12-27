0070ebe4200e93a39dbf45cf4c68821314564f7b
Michael Faria <michaelronaldfaria@gmail.com>
2025-12-27 12:47:59 +0000

    Improved readability in Tasks.jsx. Added and updated comments.

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -1,12 +1,11 @@
-import '../styles/Tasks.css'
 import {useEffect, useState} from "react";
 import {comparePriority, convertDate, convertDateToReadable, daysRemaining} from "../utils/utilFuncs";
+import '../styles/Tasks.css'
 
-// simple component that displays an HTML list of tasks from a given tasks prop
+// component that displays a table of tasks from the database
 export default function Tasks({tasks, sortValue, searchValue, filterConstraints}) {
     const [listOfTasks, setListOfTasks] = useState([]);
-    // we need the date to filter by before due date and overdue.
-    const [localDate, setLocalDate] = useState("");
+    const [localDate, setLocalDate] = useState(""); // we need the date to filter out tasks that are before the due date or overdue
     const [hiddenColumns, setHiddenColumns] = useState({
         id: false,
         title: false,
@@ -22,16 +21,17 @@ export default function Tasks({tasks, sortValue, searchValue, filterConstraints}
     // obtaining the current date
     useEffect(() => {
         // we create a new date object (dd/mm/yyyy) then replace the '/' with '-' (dd-mm-yyyy) then we reverse the order (yyyy-mm-dd)
-        // so now it matches the dates in the database allowing us to compare dates
+        // so now it matches the formats of the dates in the database (also yyyy-mm-dd) allowing us to compare today with a task's start/due date
         setLocalDate(convertDate(new Date().toLocaleDateString().replaceAll("/", "-")));
     }, [])
 
     // copying the tasks passed from the parent, then filtering and sorting the tasks
     useEffect(() => {
-        let updatedList = [...tasks]; // copy to prevent mutation ("Changing an existing object or array in place instead of creating a new one.")
+        let updatedList = [...tasks];
 
         // searching
         updatedList = updatedList.filter(task => {
+            // if the task's title or description includes the input in the search bar, we keep it
             return ((task.title).includes(searchValue) || (task.description).includes(searchValue));
         })
 
@@ -41,23 +41,23 @@ export default function Tasks({tasks, sortValue, searchValue, filterConstraints}
                 return false;
             }
 
-            if (!filterConstraints.uncompleted && task.status !== "Completed") {
+            else if (!filterConstraints.uncompleted && task.status !== "Completed") {
                 return false;
             }
 
-            if (!filterConstraints.started && task.startDate >= localDate) {
+            else if (!filterConstraints.started && task.startDate >= localDate) {
                 return false;
             }
 
-            if (!filterConstraints.notStarted && task.startDate < localDate) {
+            else if (!filterConstraints.notStarted && task.startDate < localDate) {
                 return false;
             }
 
-            if (!filterConstraints.beforeDueDate && task.dueDate >= localDate) {
+            else if (!filterConstraints.beforeDueDate && task.dueDate >= localDate) {
                 return false;
             }
 
-            if (!filterConstraints.overdue && task.dueDate < localDate) {
+            else if (!filterConstraints.overdue && task.dueDate < localDate) {
                 return false;
             }
 
@@ -67,16 +67,16 @@ export default function Tasks({tasks, sortValue, searchValue, filterConstraints}
         // sorting
         updatedList.sort((a, b) => {
             // sorting based on the selected property to sort by
-            if (typeof a[sortValue] === "number") {
+            if (typeof a[sortValue] === "number") { // if we are sorting via a number (e.g. ID) we can use arithmetic
                 return a[sortValue] - b[sortValue];
             }
 
-            if (typeof a[sortValue] === "string") {
-                if (sortValue === "priority") {
-                    return comparePriority(a[sortValue], b[sortValue]);
+            if (typeof a[sortValue] === "string") { // if we are sorting by a string (e.g. title, description) we can compare the strings lexicographically
+                if (sortValue === "priority") { // special case: priority can't be compared lexicographically, so we call a helper function that correctly compares "High", "Medium" and "Low"
+                    return comparePriority(a[sortValue], b[sortValue]); // returns negative if a is less than b, zero if a and b are equal, and positive if a is greater than b
                 }
 
-                // JS string method to compare two strings, returns -1, 0, 1 (before, equal, after)
+                // JS string method to lexicographically compare two strings, returns -1, 0, 1 (before, equal, after)
                 return a[sortValue].localeCompare(b[sortValue]);
             }
         })
@@ -86,13 +86,16 @@ export default function Tasks({tasks, sortValue, searchValue, filterConstraints}
 
     // toggling a column's visibility
     const toggleColumnVisibility = (key) => {
+        // copy the previous state, and flip the value of the given key
         setHiddenColumns(prevState => ({...prevState, [key]: !prevState[key]}));
     }
 
-    // if column is hidden replace with "...", otherwise display the given field
+    // function that either hides or displays a table cell's text content based on whether the column's visibility
+    // every <td> tag (table cell) uses this function for its text content
     const columnValue = (key, field) => hiddenColumns[key] ? "..." : field;
 
-    // determining css style to use based on task's status/priority
+    // simple JS objects to determine css style to use based on task's status/priority
+    // the table's status and priority columns use this
     const statusStyling = {"Completed": "status-completed", "Pending": "status-pending", "On Hold": "status-on-hold"};
     const priorityStyling = {"High": "priority-high", "Medium": "priority-medium", "Low": "priority-low"}
 
@@ -100,11 +103,11 @@ export default function Tasks({tasks, sortValue, searchValue, filterConstraints}
         <div id={"displayedTasks"}>
             <h3>Your tasks:</h3>
 
-            {(tasks.length === 0) ? ( // no tasks in the database
+            {(tasks.length === 0) ? ( // if there are no tasks in the database
                 <p>You have no tasks!</p>
-            ) : (listOfTasks.length === 0) ? ( // no tasks adhering to the current: searching and filtering criteria
+            ) : (listOfTasks.length === 0) ? ( // if there are no tasks adhering to the current: searching and filtering criteria
                 <p>You have no tasks that meet the current filter!</p>
-            ) : (
+            ) : ( // otherwise display the table of tasks
                 <div id="table">
                     <table>
                         <thead>
