556863baff23a72657432bb6c79e65f2714e4f07
Michael Faria <michaelronaldfaria@gmail.com>
2026-01-18 14:51:56 +0000

    Minor code reformatting. Added additional comments.

--- frontend/src/components/Notification.jsx
+++ frontend/src/components/Notification.jsx
@@ -5,7 +5,9 @@ import '../styles/Notification.css'
 export default function Notification({message, isVisible, time, onClick}) {
     const [progress, setProgress] = useState(0);
 
+    // handles animating the progress bar indicating the timeout of the notification
     useEffect(() => {
+        // if the notification is no longer visible then, we reset the progress after a 500ms delay, which is how long the notification takes to move off the screen
         if (!isVisible) {
             setTimeout(() => {
                 setProgress(0);
@@ -15,18 +17,20 @@ export default function Notification({message, isVisible, time, onClick}) {
 
         const startTime = performance.now();
 
-        function update(now) {
+        // request update call on next before browser repaint
+        requestAnimationFrame(update);
+
+        function update(now) { // now = performance.now()
             const elapsedTime = now - startTime;
             const percent = Math.min((elapsedTime / time) * 100, 100);
 
             setProgress(percent);
 
+            // while the progress bar is not full, request another update before next browser repaint
             if (percent < 100) {
                 requestAnimationFrame(update);
             }
         }
-
-        requestAnimationFrame(update);
     }, [isVisible, time]);
 
 
